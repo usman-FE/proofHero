728ceb07d604d21f605d98023ba84c783de0ebef
Muhammad Usman <ch.usman564@gmail.com>
2022-04-20 09:29:05 +0500

    tabular fix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// MOBILE HAMBURGER MENU
 const mobNavBtn = document.querySelector(".navbar-toggler");
 const mobNav = document.querySelector(".mob-nav");
 
@@ -5,3 +7,22 @@ mobNavBtn.addEventListener("click", function () {
   console.log(mobNav);
   mobNav.classList.toggle("mob-nav-show");
 });
+
+
+// MY NEW NOTIFICATION TABULAR COMPONENT
+const parentEl = document.querySelector('.accordionTopBtn');
+const btnEl = document.querySelectorAll('.accordionBtn');
+
+parentEl.addEventListener('click', function (e) {
+
+  if (!(e.target.classList.contains('accordion-button'))) return;
+  btnEl.forEach(el => {
+
+    el.classList.remove('active-accord');
+
+    if (e.target === el.firstElementChild.firstElementChild) {
+      console.log(el);
+      el.classList.add('active-accord');
+    }
+  })
+})
