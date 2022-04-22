9a3e633f02680dfb2a9e5de66efde94b02a57d3c
Usman-FE <ch.usman564@gmail.com>
2022-04-22 10:37:29 +0500

    Fix floating eye button

--- js/floatingEye.js
+++ js/floatingEye.js
@@ -1,36 +1,36 @@
-const closeBtn = document.querySelector('.previewCloseBtn');
-const notifyEl = document.querySelector('.notification-box-parent');
-const floatingEye = document.querySelector('.floating-eye');
-const previewSection = document.querySelector('.previewsTop');
-console.log(notifyEl);
-
-
+const closeBtn = document.querySelector(".previewCloseBtn");
+const notifyEl = document.querySelector(".notification-box-parent");
+const floatingEye = document.querySelector(".floating-eye");
+const previewSection = document.querySelector(".previewsTop");
+const settingsBtn = document.querySelector("#nav-settings-tab");
 
 // Close notification preview
-closeBtn.addEventListener('click', function () {
+closeBtn.addEventListener("click", function () {
   notifyEl.style.display = "none";
-})
+});
 
 // Show notification preview
-floatingEye.addEventListener('click', function () {
+floatingEye.addEventListener("click", function () {
   notifyEl.style.display = "block";
-})
+});
 
 // Show floating when preview is not visible
 const obsCallBack = (entries) => {
   const [entry] = entries;
-
-  if (!entry.isIntersecting) {
-    console.log(entry);
+  if (!entry.isIntersecting && settingsBtn.classList.contains("active")) {
+    // console.log(entry);
     floatingEye.style.display = "flex";
-    observer.unobserve(previewSection);
+    // observer.unobserve(previewSection);
+  } else {
+    floatingEye.style.display = "none";
+    notifyEl.style.display = "none";
   }
-}
+};
 
 const options = {
   root: null,
   threshold: 0,
-  rootMargin: '-60px',
+  rootMargin: "-60px",
 };
 
 const observer = new IntersectionObserver(obsCallBack, options);
